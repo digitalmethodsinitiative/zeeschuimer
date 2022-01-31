96fdcff87f12039fead5d4fe1611e8d370cd4820
Stijn Peeters <stijn.peeters@uva.nl>
2022-01-31 13:15:05 +0100

    TikTok: Capture items embedded in page

--- modules/tiktok.js
+++ modules/tiktok.js
@@ -6,16 +6,41 @@ zeeschuimer.register_module(
             return [];
         }
 
+        /**
+         * Some data is embedded in the page rather than loaded asynchronously.
+         * This here extracts it!
+         */
+        let embedded_sigil_start = /window\['SIGI_STATE']\s*=\s*/mg;
+        let embedded_sigil_end = /;\s*window\['SIGI_RETRY']\s*=\s*/mg;
+        let data;
+        let from_embed = false;
+        if(embedded_sigil_start.test(response)) {
+            response = response.split(embedded_sigil_start)[1];
+            if(!embedded_sigil_end.test(response)) {
+                return [];
+            }
+            response = response.split(embedded_sigil_end)[0];
+            from_embed = true;
+        }
+
+        if(!response) {
+            return [];
+        }
+
         try {
             data = JSON.parse(response);
         } catch (SyntaxError) {
             return [];
         }
 
-        if (!data["itemList"]) {
+        if("ItemModule" in data) {
+            let r = Object.values(data["ItemModule"]);
+            console.log(r);
+            return r;
+        } else if ("itemList" in data) {
+            return data["itemList"];
+        } else {
             return [];
         }
-
-        return data["itemList"];
     }
 )
