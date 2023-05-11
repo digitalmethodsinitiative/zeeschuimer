82f0b2c5855da889f1503f94fd4ca0e0270d5943
Dale Wahl <dalewahl@gmail.com>
2023-05-11 15:43:55 +0200

    clean up

--- modules/douyin.js
+++ modules/douyin.js
@@ -3,8 +3,7 @@ zeeschuimer.register_module(
     'douyin.com',
     function (response, source_platform_url, source_url) {
         let domain = source_platform_url.split("/")[2].toLowerCase().replace(/^www\./, '');
-
-        if (domain !== 'douyin.com' && domain !== 'live.douyin.com') { //!["douyin.com", "live.douyin.com"].includes(domain)) {
+        if (!["douyin.com"].includes(domain)) {
             return [];
         }
 
@@ -18,15 +17,8 @@ zeeschuimer.register_module(
         if (("e" in data) && ("sc" in data) && ("tc" in data) && (3 === Object.keys(data).length)) {
             // These appear to be status pings of some kind
             return [];
-        } else {
-            // debug
-            console.log("MAYBE INTERESTING")
-            console.log(data)
-        }
-
-        if ("cards" in data) {
+        } else if ("cards" in data) {
             // Front Page (首页) tab
-            console.log("Collecting Douyin Front Page data...")
             let usable_items = [];
             for(let i in data["cards"]) {
                 let item = data["cards"][i]["aweme"];
@@ -39,23 +31,28 @@ zeeschuimer.register_module(
                 }
 
             }
+            console.log(`Collected ${usable_items.length} Douyin videos from Front page tab`)
             return usable_items;
-        }
+        } else if ("aweme_list" in data) {
+            // Recommend (推荐) tab, Hot (热点) tab, and Channels (e.g. game (游戏), entertainment (娱乐), music (音乐))
 
-        // Recommend (推荐) tab, Hot (热点) tab, and Channels (e.g. game (游戏), entertainment (娱乐), music (音乐))
-        if ("aweme_list" in data) {
-            console.log("Collecting Douyin Recommend, Hot, or Channel data...")
             let usable_items = [];
             for(let i in data["aweme_list"]) {
                 let item_data = data["aweme_list"][i];
                 item_data["id"] = item_data["aweme_id"];
                 usable_items.push(item_data);
             }
+            console.log(`Collected ${usable_items.length} Douyin videos from Recommend, Hot, or Channel tabs`)
             return usable_items;
+        } else {
+            // debug
+            //console.log("MAYBE INTERESTING")
+            //console.log(data)
         }
 
         // if () {
         //     // Live Stream (直播) tab}
+        // Unable to detect this domain?
         // }
     }
 );
