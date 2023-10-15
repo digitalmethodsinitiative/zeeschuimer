ce78e1dbd0a1b1ee0856df6b9b0c2e0dbb8f09b3
Parker-Kasiewicz <110084850+Parker-Kasiewicz@users.noreply.github.com>
2023-10-15 11:25:31 -0700

    draft of gab.js
    first draft - just with posts & videos on home page

---
+++ modules/gab.js
@@ -0,0 +1,70 @@
+zeeschuimer.register_module(
+  'Gab',
+  'gab.com',
+  function (response, source_platform_url, source_url) {
+    let domain = source_platform_url.split("/")[2].toLowerCase().replace(/^www\./, '');
+
+    if (
+      !["gab.com"].inclues(domain) 
+      || (
+          source_url.indexOf('explore') < 0
+        && source_url.indexOf('video') < 0
+        )
+    ) {
+      return [];
+    }
+
+    let data;
+    let posts = [];
+    try {
+      data = JSON.parse(response);
+    } catch (SyntaxError) {
+        return [];
+    }zeeschuimer.register_module(
+    'Gab',
+    'gab.com',
+    function (response, source_platform_url, source_url) {
+      let domain = source_platform_url.split("/")[2].toLowerCase().replace(/^www\./, '');
+  
+      if (
+        !["gab.com"].inclues(domain) 
+        || (
+            source_url.indexOf('explore') < 0
+          && source_url.indexOf('video') < 0
+          )
+      ) {
+        return [];
+      }
+  
+      let data;
+      let posts = [];
+      try {
+        data = JSON.parse(response);
+      } catch (SyntaxError) {
+          return [];
+      }
+
+      let traverse = function (obj) {
+        for (let property in obj) {
+            let child = obj[property]
+            if (!child) {
+                continue;
+            }
+            if (
+                (
+                    child.hasOwnProperty('s')
+                )
+            ) {
+                for (let entry in child['s']) {
+                    post = child['s'][entry];
+                    posts.push(post);
+                }
+                
+            }
+        }
+      }
+
+      traverse(data);
+      return posts;
+    }
+  );
