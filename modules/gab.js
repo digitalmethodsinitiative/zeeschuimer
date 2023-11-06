78e6dd3947075b5f571a5cafb743f2b15b4c73c1
Parker-Kasiewicz <parkerkasiewicz@gmail.com>
2023-11-05 16:50:26 -0800

    Functionality for groups & user info

--- modules/gab.js
+++ modules/gab.js
@@ -1,24 +1,24 @@
 zeeschuimer.register_module(
-    'Gab!',
+    'Gab',
     'gab.com',
     function (response, source_platform_url, source_url) {
         let domain = source_platform_url.split("/")[2].toLowerCase().replace(/^www\./, '');
-
-        if (!["gab.com"].includes(domain) || (source_url.indexOf('search?type=status') < 0 && source_url.indexOf('explore?') < 0)) {
+        // Check if the domain is gab.com and the source_url corresponds to the expected endpoints
+        if (!["gab.com"].includes(domain)) {
             return [];
         }
 
         let data;
-        let posts = [];
-        
+        let items = []; // Could be posts, groups, or users
+
         try {
             data = JSON.parse(response);
         } catch (SyntaxError) {
             return [];
         }
 
-        if (data.s && Array.isArray(data.s)) {
-            console.log("exploring!");
+        // Handling posts in "explore"
+        if ((source_url.indexOf('explore?') >= 0 || source_url.indexOf('home?') >= 0) && data.s && Array.isArray(data.s)) {
             for (let post of data.s) {
                 let transformedPost = {
                     id: post.i,
@@ -33,12 +33,12 @@ zeeschuimer.register_module(
                       avatar: ""  
                     }
                 };
-                posts.push(transformedPost);
+                items.push(transformedPost);
             }
         }
 
-        if (data.statuses && Array.isArray(data.statuses)) {
-            console.log("searching!")
+        // Handling posts in "search"
+        if (source_url.indexOf('search?type=status') >= 0 && data.statuses && Array.isArray(data.statuses)) {
             for (let post of data.statuses) {
                 let transformedPost = {
                     id: post.id,
@@ -53,10 +53,56 @@ zeeschuimer.register_module(
                         avatar: post.account.avatar
                     }
                 };
-                posts.push(transformedPost);
+                items.push(transformedPost);
             }
         }
 
-        return posts;
+        // Handling groups
+        if (source_url.indexOf('groups') >= 0 && Array.isArray(data) && 'member_count' in data[0]) {
+            for (let group of data) {
+                let transformedGroup = {
+                    id: group.id,
+                    created_at: group.created_at,
+                    url: group.url,
+                    account: {
+                        title: group.title,
+                        description: group.description,
+                        member_count: group.member_count,
+                        is_verified: group.is_verified,
+                        is_private: group.is_private,
+                        category: group.group_category ? group.group_category.text : null
+                    }
+                };
+                items.push(transformedGroup);
+            }
+        }
+
+        // Handling user information
+        if (source_url.indexOf('/users/') >= 0 && data.hasOwnProperty('id') && data.hasOwnProperty('username') && data.username.trim() !== '') {
+            let transformedUser = {
+                id: data.id,
+                created_at: data.created_at,
+                url: data.url,
+                account: {
+                    username: data.username,
+                    display_name: data.display_name,
+                    note: data.note,
+                    avatar: data.avatar,
+                    followers_count: data.followers_count,
+                    following_count: data.following_count,
+                    statuses_count: data.statuses_count,
+                    is_pro: data.is_pro,
+                    is_verified: data.is_verified,
+                    is_donor: data.is_donor,
+                    is_investor: data.is_investor,
+                    show_pro_life: data.show_pro_life,
+                    is_parody: data.is_parody,
+
+                }
+            };
+            items.push(transformedUser);
+        }
+
+        return items;
     }
 );
