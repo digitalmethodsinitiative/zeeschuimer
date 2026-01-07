b10a83abf5b5ee350cbbdd2919560afc9b3dad32
uree <12390599+uree@users.noreply.github.com>
2026-01-07 10:22:46 +0100

    added fb comments module for downloading comments under posts

---
+++ modules/facebook-comments.js
@@ -0,0 +1,69 @@
+zeeschuimer.register_module(
+    'Facebook (comments)',
+    'facebook.com',
+    function (response, source_platform_url, source_url) {
+        let domain = source_platform_url.split("/")[2].toLowerCase().replace(/^www\./, '');
+
+        if (!["facebook.com"].includes(domain) || !source_url.endsWith('api/graphql/')) {
+            return [];
+        }
+
+        let datas = [];
+        let edges = [];
+
+        try {
+            datas.push(JSON.parse(response));
+        } catch (e) {
+            if (response.substring(0, 1) === '{') {
+                //ndjson
+                const lines = response.split('\n');
+                for (const line of lines) {
+                    try {
+                        datas.push(JSON.parse(line));
+                    } catch (e) {
+                    }
+                }
+            }
+        }
+
+        // need to save feedback id on post and comment side and story id on the comment side
+
+        const traverse = function (obj) {
+            for (const property in obj) {
+                if (!obj.hasOwnProperty(property)) {
+                    // not actually a property
+                    continue;
+                }
+
+
+                if(obj['id'] && obj['__typename'] === 'Feedback' && obj?.comment_rendering_instance_for_feed_location?.comments?.edges?.length > 0) {
+                    const exists = edges.some(edge => edge.id === obj.id);
+                    if (!exists) {
+                        edges.push(obj);
+                    }
+                } else if (typeof (obj[property]) === "object") {
+                    traverse(obj[property]);
+                }
+            }
+        }
+
+
+        for (const data of datas) {
+            if (data) {
+                traverse(data);
+            }
+        }
+
+        // this runs at the very end
+        for(const index in edges) {
+            try {
+                const better_id = atob(edges[index]['id']);  // decode base64
+                edges[index]['id'] = better_id;
+            } catch (e) {
+                // pass
+            }
+        }
+        return edges;
+    },
+    'facebook-comments'
+);
